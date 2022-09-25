39612b7d4937a690aa1c966f1ef679a642c7a617
marinanc <marinancarrizo@gmail.com>
2022-09-25 19:30:33 -0300

    feat: show data on the display

--- Display.js
+++ Display.js
@@ -8,6 +8,17 @@ class Display {
     }
 
     agregarNumero(numero){
-        this.valorActual = numero;
+        //Si es el punto y ya hay un punto en el display, no hacer nada
+        if(numero === '.' && this.valorActual.includes('.')) return
+        //Si es el punto y no hay nada en el display, no hacer nada
+        if(numero === '.' && this.valorActual === '') return
+        
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = this.valorAnterior;
     }
 }
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-const calculadora = new Calculadora();
+const display = new Display(displayValorAnterior, displayValorActual);
 
-console.log(calculadora.sumar(2,3));
+/* Cada vez que se hace click en un boton llama a la funcion del display
+para agregar numero, enviando como parámetro lo que está en el HTML*/
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
