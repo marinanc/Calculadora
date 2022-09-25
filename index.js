0b7136839da46c9a58eb708883fe08fb69b14d21
marinanc <marinancarrizo@gmail.com>
2022-09-25 18:12:48 -0300

    feat: Operations with numbers

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
+
+const calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3));
